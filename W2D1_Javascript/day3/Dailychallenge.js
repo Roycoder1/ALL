e9619c25e1c7444b694ddf796e74b833f7fbc437
BENISTI <benisti@MacBook-Air-de-BENISTI.local>
2022-06-28 22:44:55 +0300

    added all

--- W2D1_Javascript/day3/Dailychallenge.js
+++ W2D1_Javascript/day3/Dailychallenge.js
@@ -1,7 +1,20 @@
-let stars= ['*','*','*','*','*','*'];
+let stars= ['*','**','***','****','*****','******'];
 
 for(let i=0;i<6;i++)
 {
 	
 	console.log(stars[i]);
+}
+
+
+
+
+
+let stars= [['*'],['**'],['***'],['****'],['*****'],['******']];
+
+
+for (var i=0; i < stars.length; i++) {
+	for (var j=0; j < stars[i].length; j++) {
+		console.log(stars[i][j]);
+	}
 }
