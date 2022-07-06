365553331af8ef16296a58bab53c3f1307408c11
BENISTI <benisti@MacBook-Air-de-BENISTI.local>
2022-07-06 23:03:34 +0300

    add

--- W3D1/day4/daily.js
+++ W3D1/day4/daily.js
@@ -1,7 +1,48 @@
-let listTasks = [ ]
+let listTasks = []
 
-function addTask (){
+function getValue(){
 
 
-	
+	let input = document.querySelector('input')
+	input.value
+	console.log(input.value)
+
+
+
+	if (input.value=='') {
+		console.log(input.value)
+		alert('this value is empty')
+
+
+	}
+	else{
+		input = input.value;
+		
+		listTasks.push(input)
+		
+		let lists = document.querySelector('.listTasks')
+		
+		let text = listTasks.toString()
+		
+
+
+
+
+		let createDiv = document.createElement('div')
+		let create = document.createElement('input');
+		create.setAttribute('type', 'checkbox')
+		// create.classList ='fa-solid fa-x';
+		createDiv.innerText = ' X  '+ text
+		console.log(createDiv)
+		lists.appendChild(createDiv)
+		lists.appendChild(create)
+		console.log(lists)
+
+	}
 }
+
+getValue()
+
+
+
+
