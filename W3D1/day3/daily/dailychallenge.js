e27e4a1d4aff3bd611e32fe7d394373bee1c4210
BENISTI <benisti@MacBook-Air-de-BENISTI.local>
2022-07-06 14:59:09 +0300

    commit

--- W3D1/day3/daily/dailychallenge.js
+++ W3D1/day3/daily/dailychallenge.js
@@ -1,24 +1,32 @@
 
-function getValue(){
+function getValue(event){
 
 
-	let text = document.querySelector('input').value;
+	let text = document.querySelector('input');
 	console.log(text);
-	let input= document.querySelector('input')
+	
 	
 
-	let regex = /[^a-zA-Z0-9]/g
-	let isValid =regex.test(text);
+	let regex = /[^a-zA-Z]/g
+	let isValid =regex.test(text.value);
+	console.log(isValid)
+
+	if (isValid) {
+
+		// event.target.value = event.target.value.slice(0,-1)
+		text.value = text.value.replace(regex,"")
 
 
-	if (!isValid) {
-
-		return true;
 	}
-	else{
-		text = text.replace(/[^a-zA-Z0-9]/g, '')
-	}
-	console.log('false')
+	// if (!isValid) {
+
+	// 	console.log('true')
+	// }
+	// else{
+	// 	text = text.replace(/[^a-zA-Z0-9]/g, '')
+	// 	console.log('false')
+	// }
+	
 }
 
 
