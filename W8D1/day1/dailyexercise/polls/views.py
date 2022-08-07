cffd85e53afe8657ecdf7f8a63ada86b07cbd341
BENISTI <benisti@MacBook-Air-de-BENISTI.local>
2022-08-07 23:04:09 +0300

    ADD

---
+++ W8D1/day1/dailyexercise/polls/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    people = [
+  {
+    'id': 1,
+    'name': 'Bob Smith',
+    'age': 35,
+    'country': 'USA'
+  },
+  {
+    'id': 2,
+    'name': 'Martha Smith',
+    'age': 60,
+    'country': 'USA'
+  },
+  {
+    'id': 3,
+    'name': 'Fabio Alberto',
+    'age': 18,
+    'country': 'Italy'
+  },
+  {
+    'id': 4,
+    'name': 'Dietrich Stein',
+    'age': 85,
+    'country': 'Germany'
+  }
+]
+    return render(request,'index1.html',{'peoples' : people})
